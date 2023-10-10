b0ca25e494924e84f8df4b9e0e025b5c6d4f623a
hiteshmujwani <hiteshmujwaniofficial@gmail.com>
2023-10-10 19:43:17 +0530

    changes

--- animations.js
+++ animations.js
@@ -1,8 +1,10 @@
 let navbar = document.getElementById('navlinks');
 let menubtn = document.getElementById('menu-btn');
-
+function change(x){
+    x.classList.toggle("fa-solid fa-xmark")
+}
 function show(){
-    // x.classList.toggle("fa-xmark")
+    // x.classList.toggle("fa-solid fa-xmark")
     if(navbar.style.top == '-100%'){
         navbar.style.top = '0%';
         
@@ -12,5 +14,6 @@ function show(){
         navbar.style.top = '-100%';
 
     }
+    
 }
     
